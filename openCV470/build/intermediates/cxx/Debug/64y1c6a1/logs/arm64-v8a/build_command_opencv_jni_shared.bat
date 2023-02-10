9d33fe4dd5abc2a2dc54d7df711953a2d8b249e2
Vladimir <wolk.1221@gmail.com>
2023-02-10 20:18:29 +0300

    Начальный коммит
    Начальная точка, ещё надо посмотреть на что он ругается тут:
    E/OpenGLRenderer: Unable to match the desired swap behavior.

---
+++ openCV470/build/intermediates/cxx/Debug/64y1c6a1/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\AndroidSDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\wolk1\\AndroidStudioProjects\\MyOpenCV_1\\openCV470\\.cxx\\Debug\\64y1c6a1\\arm64-v8a" ^
+  opencv_jni_shared
---
+++ openCV470/build/intermediates/cxx/Debug/64y1c6a1/logs/x86_64/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\AndroidSDK\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\wolk1\\AndroidStudioProjects\\MyOpenCV_1\\openCV470\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86_64" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86_64" ^
+  "-DANDROID_NDK=C:\\AndroidSDK\\ndk\\23.1.7779620" ^
+  "-DCMAKE_ANDROID_NDK=C:\\AndroidSDK\\ndk\\23.1.7779620" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\AndroidSDK\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\AndroidSDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\wolk1\\AndroidStudioProjects\\MyOpenCV_1\\openCV470\\build\\intermediates\\cxx\\Debug\\64y1c6a1\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\wolk1\\AndroidStudioProjects\\MyOpenCV_1\\openCV470\\build\\intermediates\\cxx\\Debug\\64y1c6a1\\obj\\x86_64" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\wolk1\\AndroidStudioProjects\\MyOpenCV_1\\openCV470\\.cxx\\Debug\\64y1c6a1\\x86_64" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
